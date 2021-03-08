d293227a41415fc333b130961657d9c9407457b9
Ariel <ariel@ariels-mbp.attlocal.net>
2021-03-07 22:40:00 -0500

    the final draft

--- develop/script.js
+++ develop/script.js
@@ -1,23 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 // generate password function
-
 function generatePassword() {
-  var lengthofpassword = prompt("please choose a length of at least 8 characters and no more than 128 characters")
-
-
-
+  var lengthofpassword = prompt("please choose a length of at least 8 characters and no more than 128 characters");
   if (lengthofpassword >= 8 && lengthofpassword <= 128) {
     // alerts
     var resultspecial = confirm("click ok to confirm using special characters");
@@ -30,72 +23,55 @@ function generatePassword() {
     );
     return "";
   }
-
   if (resultspecial || resultnumeric || resultlower || resultupper) {
   } else {
     alert("must have a least 1 special condition");
     return "";
   }
-
   var creatingpassword = [];
   var counter = 1;
-
-  for (let index = 1; index <= lengthofpassword; index = counter) 
-  {
+  for (let index = 1; index <= lengthofpassword; index = counter) {
     if (resultspecial == true && counter <= lengthofpassword) {
       getrandomsymbol();
       counter++;
     }
+    if (resultnumeric == true && counter <= lengthofpassword) {
+      getnumber();
+      counter++;
+    }
+    if (resultlower == true && counter <= lengthofpassword) {
+      getlower();
+      counter++;
+    }
+    if (resultupper == true && counter <= lengthofpassword) {
+      getupper();
+      counter++;
+    }
   }
-
-  if (resultnumeric == true && counter <= lengthofpassword) {
-    getnumber();
-    counter++;
-  }
-
-  if (resultlower == true && counter <= lengthofpassword) {
-    getlower();
-    counter++;
-  }
-  if (resultupper == true && counter <= lengthofpassword) {
-    getupper();
-    counter++;
-  }
-
   // Random char
   function getrandomsymbol() {
-    var symbol = "!@#$%^&*(){}=<>/,.?;[]"
+    var symbol = "!@#$%^&*(){}=<>/,.?';[]"
     var holdsymbol = symbol[Math.floor(Math.random() * symbol.length)];
-
     creatingpassword.push(holdsymbol);
   }
-
   // Random Uppercase
   function getupper() {
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var holdupper = uppercase[Math.floor(math.random() * uppercase.length)];
-
+    var holdupper = uppercase[Math.floor(Math.random() * uppercase.length)];
     creatingpassword.push(holdupper);
   }
-
-
-
-  // Random Lower
+  // // Random Lower
   function getlower() {
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var holdlower = lowercase[Math.floor(math.random() * lowercase.length)];
-
+    var holdlower = lowercase[Math.floor(Math.random() * lowercase.length)];
     creatingpassword.push(holdlower);
   }
-
   // Random number
   function getnumber() {
     var number1 = "1234567890";
-    var holdnumber = number[Math.floor(math.random() * number1.length)];
-
+    var holdnumber = number1[Math.floor(Math.random() * number1.length)];
     creatingpassword.push(holdnumber);
   }
-
   creatingpassword.sort(function (a, b) {
     return 0.5 - Math.random();
   });
